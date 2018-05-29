2ff69e7bedfa18769321a94a1d69aa56aadfc140
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-05-29 15:34:54 +0200

    Added calculator classes

--- Rekenmachine/Program.cs
+++ Rekenmachine/Program.cs
@@ -9,25 +9,6 @@ namespace Rekenmachine
     {
         static void Main()
         {
-            //var a = new Request();
-            //var b = new Request();
-            //var c = new Request();
-            //var x = new Request();
-            //var y = new Request();
-            //a.Val = 2;
-            //b.Val = 3;
-
-            //x.LeftHand = a;
-            //x.Operation = OperationType.product;
-            //x.RightHand = b;
-
-            //y.LeftHand = x;
-            //y.Operation = OperationType.product;
-            //y.RightHand = new Request(){Val = 5};        
-
-
-            //Console.WriteLine(y.Val);
-
             bool keepActive;
             do
             {
@@ -35,7 +16,7 @@ namespace Rekenmachine
                 string calculationString = Message.InputMessage();
                 
                 var req = new Request(calculationString);              
-                
+                Console.WriteLine(req.Val);
                 //Output.Message(req.());
 
                 // keep active                
--- Rekenmachine/Request.cs
+++ Rekenmachine/Request.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.CodeDom;
-using System.Diagnostics.Eventing.Reader;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Rekenmachine
 {
@@ -12,6 +8,7 @@ namespace Rekenmachine
         public Request LeftHand { get; set; }
         public Request RightHand { get; set; }
         public OperationType Operation { get; set; }
+
         public decimal Val
         {
             get
@@ -27,11 +24,6 @@ namespace Rekenmachine
         }
 
         public string RequestAsString { get; set; }
-
-        public Request()
-        {
-            
-        }
 
         public Request(string input)
         {
@@ -62,6 +54,7 @@ namespace Rekenmachine
 
             return OperationType.addition;
         }
+
         private void Parse()
         {
             string matchPattern = @"^([0-9]+)([*\/+-])([\.\s\(\)0-9*\/+-]*)";
@@ -71,68 +64,14 @@ namespace Rekenmachine
             {
                 this.Val = decimal.Parse(y.Groups[1].ToString());
             }
+
             var x = Regex.Match(RequestAsString, matchPattern);
             if (x.Success)
             {
-                //LeftHand.Val = decimal.Parse(x.Groups[1].ToString());
                 LeftHand = new Request(x.Groups[1].ToString());
                 Operation = GetOperator(x.Groups[2].ToString());
                 RightHand = new Request(x.Groups[3].ToString());
             }
         }
-
-        /*
-             * new a
-             * a.lefthand = group[1]
-             * a.operator = group[2]
-             * new b
-             * b.lefthand = group[3]
-             * b.operator = group[4]
-             * b.righthand= group[5]
-             * a.righthand = b
-             *
-             *
-             */
-
-        /*
-         * readword()
-         * - int
-         * - operator
-         * - whitespace (trim)
-         *
-         * <script type="text/javascript">  
-        var str = "Field1 * (Field2+ Field3) - Field4";   
-        var matchPattern = /[^\s()*%\/ +-]+/ g;
-        var splitPattern = /[\s() *%\/ +-] +/ g;
-
-        document.write(str.match(matchPattern));
-        document.write("<br />");
-        document.write(str.split(splitPattern));
-            </ script >
-         *
-         */
-
-        /*
-        string s = new string();
-        s.Split()
-
-        Console.Write(q.rightHand.leftHand.val);
-        */
-        //public override string ToString()
-        //{
-        //    return LeftHand.ToString() + Operation.ToString() + RightHand.ToString();
-        //}
     }
-
-
-
-    public class MyTests
-    {
-        public bool TestEmptyInputShouldFailElegantly()
-        {
-            Request req = new Request("3*");
-            return (req.Calculate() == 6);
-        }
-    }
-    
 }
